96cc27e00c1993b5d3e67034ddf62e828673ba9f
rodrush <rodneymarks32@gmail.com>
2022-09-09 12:23:29 +0300

    Learning C, Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,23 @@
-#include <stdio.h>                                                           
-/**                                                                            
- *main - Entry point
- *                                                                         
- * Return: Always 0 (Success) 
- */                                                                      
-int main(void)                                                                                                                          
-{                                                           
-	char alphabet = 'A';                                                                                                                    
-	      while (alphabet <= 'Z')                                                                                                           
- 	      {                                         		               
-	putchar(alphabet);                                                                                                      
- 	 alphabet++;                                        
-   }
-	      int ch;
-	      for (ch = 'a'; ch<='z'; ch++)
-	      {
-		      	putchar(ch);
-	      }                                                                                                                                    
-	               putchar('\n');                                                                                                               
-		        return (0);                                                                                                          
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower and uppercase
+ *
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
