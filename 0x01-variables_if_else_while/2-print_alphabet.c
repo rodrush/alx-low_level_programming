0d518d77c6b434688d418fef23f18842bf86827b
rodrush <rodneymarks32@gmail.com>
2022-09-09 12:20:47 +0300

    Learning C, Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - print alphabet in lower and uppercase
+ * main - Entry point
  *
- *
- * Return: (0) Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char ch;
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+
 return (0);
 }
