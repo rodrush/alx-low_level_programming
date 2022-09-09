d6dd479da5cb66286d9daad7e98f39ec6f3d7f70
rodrush <rodneymarks32@gmail.com>
2022-09-09 02:29:37 -0700

    Learning C, Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
+
+
 /**
-* main - Print all possible combinations of single digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ *
+ * * main - Print all single digit numbers
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
 int main(void)
+
 {
-		int number;
 
-		for (number = 0; number <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		}
-		putchar('\n');
+		int digit;
 
-	return (0);
+
+
+			for (digit = 0; digit < 10; digit++)
+
+					printf("%i", digit);
+
+				putchar('\n');
+
+
+
+					return (0);
+
 }
