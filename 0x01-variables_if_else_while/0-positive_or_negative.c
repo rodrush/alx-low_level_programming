f1cdc5f4ff9ee1514dd6f119c90d4dca162d2217
rodrush <rodneymarks32@gmail.com>
2022-09-09 01:09:04 -0700

    Learning C, Variables, if, else, while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	      int n;
+
+
+
+	            srand(time(0));
+
+		          n = rand()  - RAND_MAX / 2;
+
+
+
+			        printf("%d \n", n);
+
+
+
+				      if (n > 0)
+
+					            
+
+					                 printf("The number is positive \n");
+
+				            
+
+				            else if (n < 0)
+
+						          
+
+						               printf("The number is negative \n");
+
+					          
+
+					          else
+
+							        
+
+							             printf("The number is zero \n");
+
+						         
+
+						        return 0;
+
+}
