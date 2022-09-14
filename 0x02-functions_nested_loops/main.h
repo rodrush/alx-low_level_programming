fd9f0ff302a8f66fdb683deb917ed17b2d5cdc23
rodrush <rodneymarks32@gmail.com>
2022-09-14 13:28:50 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,9 +15,5 @@ void pr_digit(int charcode, int fmt);
 int add(int a, int b);
 void pr_with_fmt(int charcode, int fmt);
 void print_to_98(int n);
-void pr_3_digit(int charcode, int fmt);
-void print_times_table(int n);
-void print_spaces(int n);
-void print_zero(void);
 
 #endif
